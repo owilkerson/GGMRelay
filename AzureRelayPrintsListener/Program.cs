6a1428343df6062624b4090446fdcd3ad8a90f0b
owilkerson <owilkerson@generalgrind.com>
2025-08-27 10:06:27 -0500

    Initial Program Structure for HTTP Azure Relay

--- AzureRelayPrintsListener/Program.cs
+++ AzureRelayPrintsListener/Program.cs
@@ -5,6 +5,7 @@
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Relay;
+    using System.Net;
 
     public class Program
     {
@@ -20,8 +21,6 @@
 
         private static async Task RunAsync()
         {
-            var cts = new CancellationTokenSource();
-
             var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(KeyName, Key);
             var listener = new HybridConnectionListener(new Uri(string.Format("sb://{0}/{1}", RelayNamespace, ConnectionName)), tokenProvider);
 
@@ -30,81 +29,32 @@
             listener.Offline += (o, e) => { Console.WriteLine("Offline"); };
             listener.Online += (o, e) => { Console.WriteLine("Online"); };
 
+            // Provide an HTTP request handler
+            listener.RequestHandler = (context) =>
+            {
+                // Do something with context.Request.Url, HttpMethod, Headers, InputStream...
+                context.Response.StatusCode = HttpStatusCode.OK;
+                context.Response.StatusDescription = "OK";
+                using (var sw = new StreamWriter(context.Response.OutputStream))
+                {
+                    sw.WriteLine("hello!");
+                }
+
+                // The context MUST be closed here
+                context.Response.Close();
+            };
+
             // Opening the listener establishes the control channel to
             // the Azure Relay service. The control channel is continuously 
             // maintained, and is reestablished when connectivity is disrupted.
-            await listener.OpenAsync(cts.Token);
+            await listener.OpenAsync();
             Console.WriteLine("Server listening");
 
-            // Provide callback for a cancellation token that will close the listener.
-            cts.Token.Register(() => listener.CloseAsync(CancellationToken.None));
-
             // Start a new thread that will continuously read the console.
-            new Task(() => Console.In.ReadLineAsync().ContinueWith((s) => { cts.Cancel(); })).Start();
-
-            // Accept the next available, pending connection request. 
-            // Shutting down the listener allows a clean exit. 
-            // This method returns null.
-            while (true)
-            {
-                var relayConnection = await listener.AcceptConnectionAsync();
-                if (relayConnection == null)
-                {
-                    break;
-                }
-
-                ProcessMessagesOnConnection(relayConnection, cts);
-            }
+            await Console.In.ReadLineAsync();
 
             // Close the listener after you exit the processing loop.
-            await listener.CloseAsync(cts.Token);
-        }
-
-        private static async void ProcessMessagesOnConnection(HybridConnectionStream relayConnection, CancellationTokenSource cts)
-        {
-            Console.WriteLine("New session");
-
-            // The connection is a fully bidrectional stream. 
-            // Put a stream reader and a stream writer over it.  
-            // This allows you to read UTF-8 text that comes from 
-            // the sender, and to write text replies back.
-            var reader = new StreamReader(relayConnection);
-            var writer = new StreamWriter(relayConnection) { AutoFlush = true };
-            while (!cts.IsCancellationRequested)
-            {
-                try
-                {
-                    // Read a line of input until a newline is encountered.
-                    var line = await reader.ReadLineAsync();
-
-                    if (string.IsNullOrEmpty(line))
-                    {
-                        // If there's no input data, signal that 
-                        // you will no longer send data on this connection.
-                        // Then, break out of the processing loop.
-                        await relayConnection.ShutdownAsync(cts.Token);
-                        break;
-                    }
-
-                    // Write the line on the console.
-                    Console.WriteLine(line);
-
-                    // Write the line back to the client, prepended with "Echo:"
-                    await writer.WriteLineAsync($"Echo: {line}");
-                }
-                catch (IOException)
-                {
-                    // Catch an I/O exception. This likely occurred when
-                    // the client disconnected.
-                    Console.WriteLine("Client closed connection");
-                    break;
-                }
-            }
-
-            Console.WriteLine("End session");
-
-            // Close the connection.
-            await relayConnection.CloseAsync(cts.Token);
+            await listener.CloseAsync();
         }
     }
 }
--- AzureRelayPrintsSender/Program.cs
+++ AzureRelayPrintsSender/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.Azure.Relay;
 
 namespace Client
@@ -20,60 +21,19 @@ namespace Client
 
         private static async Task RunAsync()
         {
-            Console.WriteLine("Enter lines of text to send to the server with ENTER");
-
-            // Create a new hybrid connection client.
-            var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(KeyName, Key);
-            var client = new HybridConnectionClient(new Uri(String.Format("sb://{0}/{1}", RelayNamespace, ConnectionName)), tokenProvider);
-
-            // Initiate the connection.
-            var relayConnection = await client.CreateConnectionAsync();
-
-            // Run two concurrent loops on the connection. One 
-            // reads input from the console and then writes it to the connection 
-            // with a stream writer. The other reads lines of input from the 
-            // connection with a stream reader and then writes them to the console. 
-            // Entering a blank line shuts down the write task after 
-            // sending it to the server. The server then cleanly shuts down
-            // the connection, which terminates the read task.
-
-            var reads = Task.Run(async () => {
-                // Initialize the stream reader over the connection.
-                var reader = new StreamReader(relayConnection);
-                var writer = Console.Out;
-                do
-                {
-                    // Read a full line of UTF-8 text up to newline.
-                    string line = await reader.ReadLineAsync();
-                    // If the string is empty or null, you are done.
-                    if (String.IsNullOrEmpty(line))
-                        break;
-                    // Write to the console.
-                    await writer.WriteLineAsync(line);
-                }
-                while (true);
-            });
-
-            // Read from the console and write to the hybrid connection.
-            var writes = Task.Run(async () => {
-                var reader = Console.In;
-                var writer = new StreamWriter(relayConnection) { AutoFlush = true };
-                do
-                {
-                    // Read a line from the console.
-                    string line = await reader.ReadLineAsync();
-                    // Write the line out, also when it's empty.
-                    await writer.WriteLineAsync(line);
-                    // Quit when the line is empty.
-                    if (String.IsNullOrEmpty(line))
-                        break;
-                }
-                while (true);
-            });
-
-            // Wait for both tasks to finish.
-            await Task.WhenAll(reads, writes);
-            await relayConnection.CloseAsync(CancellationToken.None);
+           var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(
+            KeyName, Key);
+            var uri = new Uri(string.Format("https://{0}/{1}", RelayNamespace, ConnectionName));
+            var token = (await tokenProvider.GetTokenAsync(uri.AbsoluteUri, TimeSpan.FromHours(1))).TokenString;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = uri,
+                Method = HttpMethod.Get,
+            };
+            request.Headers.Add("ServiceBusAuthorization", token);
+            var response = await client.SendAsync(request);
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
         }
     }
 }
